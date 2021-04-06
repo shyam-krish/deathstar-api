329198aa91898ddfeb212e8983f236bf1a2d3941
Shyam Krishnan <shyam.krishnan.us@gmail.com>
2021-04-05 22:18:45 -0400

    Setting settings explicitly in wsgi

--- deathstar/wsgi.py
+++ deathstar/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deathstar.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = "deathstar.settings"
 
 application = get_wsgi_application()
